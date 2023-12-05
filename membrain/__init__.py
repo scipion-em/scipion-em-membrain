8eb401f7473dc9130d782ed4068608827c3fe796
Ricardo Righetto <ricardorighetto@gmail.com>
2023-12-05 22:20:19 +0100

    Fixed initial version of installation

--- membrain/__init__.py
+++ membrain/__init__.py
@@ -24,24 +24,43 @@
 # *
 # **************************************************************************
 import os.path
+from datetime import datetime as dt
 
 import pwem
 from scipion.install.funcs import VOID_TGZ
 
-# Environment variables are imported from here:
-from .constants import *
+# # Environment variables are imported from here:
+# from .constants import *
 
 _logo = "icon.png"
 _references = ['lamm_membrain_2022']
 __version__ = "0.1.0"
 
+MB_SEG_VERSION = 'git'
+
+# Use this variable to activate an environment from the Scipion conda
+MEMBRAIN_SEG_ENV_VAR = "MEMBRAIN_SEG_ENV"
+DEFAULT_MEMBRAIN_SEG_ENV = "membrain-seg"
+# Use this general activation variable when installed outside Scipion:
+MEMBRAIN_SEG_ENV_ACTIVATION_VAR = "MEMBRAIN_SEG_ENV_ACTIVATION"
+
+# models
+MODEL_VERSION = '10'
+MODEL_PKG_NAME = 'membrain-seg-models'
+MEMBRAIN_SEG_MODEL_VAR = 'MEMBRAIN_SEG_MODEL'
+MEMBRAIN_SEG_MODEL = 'MemBrain_seg_v10_alpha.ckpt'
+GDRIVE_FILEID = '1tSQIz_UCsQZNfyHg0RxD-4meFgolszo8'
+
+
 class Plugin(pwem.Plugin):
-    
+
     @classmethod
     def _defineVariables(cls):
         """ Defines variables for this plugin. scipion3 config -p membrain will show them with current values"""
         cls._defineVar(MEMBRAIN_SEG_ENV_VAR, DEFAULT_MEMBRAIN_SEG_ENV)
-        cls._defineVar(MEMBRAIN_SEG_ENV_ACTIVATION_VAR, cls.getMemBrainSegActivation())
+        cls._defineVar(MEMBRAIN_SEG_ENV_ACTIVATION_VAR,
+                       cls.getMemBrainSegActivation())
+        cls._defineVar(MEMBRAIN_SEG_MODEL_VAR, MEMBRAIN_SEG_MODEL)
 
     @classmethod
     def getMemBrainSegActivation(cls):
@@ -60,12 +79,25 @@ class Plugin(pwem.Plugin):
     @classmethod
     def defineBinaries(cls, env):
 
+        def getCondaInstallation(version):
+
+            installationCmd = cls.getCondaActivationCmd()
+            installationCmd += 'conda create -y --name ' + \
+                cls.getVar(MEMBRAIN_SEG_ENV_VAR) + ' python=3.9 && '
+            installationCmd += cls.getMemBrainSegActivation() + ' && '
+            installationCmd += 'cd membrain-seg && '
+            installationCmd += 'pip install . && '
+            installationCmd += 'touch ../env-created.txt'
+
+            return installationCmd
+
         def defineMemBrainSegInstallation(version):
             installed = "last-pull-%s.txt" % dt.now().strftime("%y%h%d-%H%M%S")
 
             membrain_commands = [
                 ('git clone https://github.com/teamtomo/membrain-seg.git', 'membrain-seg'),
-                ('cd membrain-seg && git pull && touch ../%s' % installed, installed),
+                ('cd membrain-seg && git pull && touch ../%s' %
+                 installed, installed),
                 (getCondaInstallation(version), 'env-created.txt')
             ]
 
@@ -74,17 +106,19 @@ class Plugin(pwem.Plugin):
                            tar=VOID_TGZ,
                            default=True)
 
-        def getCondaInstallation(version):
-            installationCmd = cls.getCondaActivationCmd()
-            installationCmd += 'conda create --name ' + DEFAULT_MEMBRAIN_SEG_ENV + ' python=3.9 && '
-            installationCmd += cls.getMemBrainActivation() + ' && '
-            installationCmd += 'cd membrain-seg && '
-            installationCmd += 'pip install . && '
-            installationCmd += 'touch ../env-created.txt'
-
         def getModelInstallation(model_version):
 
             # wget download line obtained from: https://stackoverflow.com/a/39087286
-            modelInstallationCmd = 'curl -L -o ' + MEMBRAIN_SEG_MODEL + ' "https://drive.google.com/uc?export=download&id=' + FILEID + '&confirm=yes"'
+            modelInstallationCmd = 'curl -L -o ' + MEMBRAIN_SEG_MODEL + \
+                ' "https://drive.google.com/uc?export=download&id=' + GDRIVE_FILEID + '&confirm=yes" && '
+            modelInstallationCmd += 'touch model-downloaded.txt'
 
-            env.addPackage('membrain-seg_models', version=model_version, commands=modelInstallationCmd, tar=VOID_TGZ, default=True)
+            env.addPackage(MODEL_PKG_NAME, version=model_version,
+                           commands=[
+                               (modelInstallationCmd, 'model-downloaded.txt')],
+                           tar=VOID_TGZ,
+                           default=True)
+
+        defineMemBrainSegInstallation(MB_SEG_VERSION)
+
+        getModelInstallation(MODEL_VERSION)
