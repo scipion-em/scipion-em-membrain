635a6b11fdee016e191387c2045fc9976b489d67
Ricardo Righetto <ricardorighetto@gmail.com>
2024-03-25 15:44:09 +0100

    Fixed model loading bug

--- membrain/__init__.py
+++ membrain/__init__.py
@@ -44,7 +44,6 @@ class Plugin(pwem.Plugin):
     def _defineVariables(cls):
         """ Defines variables for this plugin. scipion3 config -p membrain will show them with current values"""
         cls._defineVar(MEMBRAIN_SEG_ENV_VAR, DEFAULT_MEMBRAIN_SEG_ENV)
-        cls._defineVar(MEMBRAIN_SEG_MODEL_VAR, MEMBRAIN_SEG_MODEL)
 
     @classmethod
     def getMemBrainSegActivation(cls):
