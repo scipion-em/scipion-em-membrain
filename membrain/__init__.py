12b57fa6d9a6d51c38decb700e8fc4b84d2ec956
Ricardo Righetto <ricardorighetto@gmail.com>
2024-05-21 19:17:09 +0200

    Fixed installation bug related to MEMBRAIN_SEG_ENV_VAR

--- membrain/__init__.py
+++ membrain/__init__.py
@@ -43,6 +43,7 @@ class Plugin(pwem.Plugin):
     @classmethod
     def _defineVariables(cls):
         """ Defines variables for this plugin. scipion3 config -p membrain will show them with current values"""
+        cls._defineVar(MEMBRAIN_SEG_ENV_VAR, DEFAULT_MEMBRAIN_SEG_ENV)
         cls._defineVar(MEMBRAIN_SEG_ENV_ACTIVATION_VAR, DEFAULT_MEMBRAIN_SEG_ENV_ACTIVATION)
         cls._defineVar(MEMBRAIN_SEG_MODEL_VAR, DEFAULT_MEMBRAIN_SEG_MODEL)
 
